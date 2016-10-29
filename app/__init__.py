a7e4c5006becc7df8efb14961e3cb006e5acf0e9
andrew <andrew.ahlers@gmail.com>
2016-10-29 18:47:34 -0400

    Chapter 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
+from pprint import pprint
+pprint(app.config['PURP'])
 # app = Flask() comes first to eliminate circular deps.
 # Views may bepends on the above variable.
 from app import views
